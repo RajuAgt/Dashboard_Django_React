09a02d9c33b3d3ce0e9ae001d5e8a32ad91013ef
RajuAgt <raju.agt501@gmail.com>
2020-04-12 17:57:08 +0530

    Modified UI for Action functionality

--- frontend/blog_frontend/src/components/Actions/Action/Action.js
+++ frontend/blog_frontend/src/components/Actions/Action/Action.js
@@ -1,20 +1,100 @@
 import React from "react";
 
 import cssClass from "./Action.css";
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Chip from '@material-ui/core/Chip';
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '70%',
+    margin: '25px auto',
+    padding: 0,
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
+  icon: {
+    verticalAlign: 'bottom',
+    height: 20,
+    width: 20,
+  },
+  details: {
+    alignItems: 'center',
+  },
+  column: {
+    flexBasis: '33.33%',
+  },
+  columnBody: {
+    flexBasis: '66.66%',
+  },
+  helper: {
+    borderLeft: `2px solid ${theme.palette.divider}`,
+    padding: theme.spacing(1, 2),
+  },
+  link: {
+    color: theme.palette.primary.main,
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+  },
+}));
+
 
 const action = props => {
+    const classes = useStyles();
+
     return (
-        <div className={cssClass.Action}>
-            <div className={cssClass.Detail}>
-                <strong>Name:</strong> {props.actionName}
-            </div>
-            <div className={cssClass.Body}>
-                <strong>Sub Action:</strong> {props.actionBody}
-            </div>
-            <div className={cssClass.Detail}>
-                <strong>On:</strong>{" "}
-                {new Date(props.publishedOn).toDateString()}
-            </div>
+      <div>
+      <div className={classes.root}>
+      <ExpansionPanel >
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1c-content"
+          id="panel1c-header"
+        >
+          <div className={classes.column}>
+            <Typography className={classes.heading}>Status</Typography>
+          </div>
+          <div className={classes.column}>
+            <Typography className={classes.secondaryHeading}>{props.actionName}</Typography>
+          </div>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails className={classes.details}>
+          <div className={classes.columnBody}>
+              {props.actionBody}
+          </div>
+          <div className={clsx(classes.column, classes.helper)}>
+            <Typography variant="caption">
+              Target Date:
+              <br />
+              {new Date(props.publishedOn).toDateString()}
+            </Typography>
+          </div>
+        </ExpansionPanelDetails>
+        <Divider />
+        <ExpansionPanelActions>
+          <Button size="small">Cancel</Button>
+          <Button size="small" color="primary">
+            Completed
+          </Button>
+        </ExpansionPanelActions>
+      </ExpansionPanel>
+    </div>
         </div>
     );
 };
