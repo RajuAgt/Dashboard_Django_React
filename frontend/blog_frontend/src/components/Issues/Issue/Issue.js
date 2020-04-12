886e7c62cb1947795ee08a10db50035a0e318cb9
RajuAgt <raju.agt501@gmail.com>
2020-04-12 17:03:28 +0530

    Modified UI Changes to Issue functionality

--- frontend/blog_frontend/src/components/Issues/Issue/Issue.js
+++ frontend/blog_frontend/src/components/Issues/Issue/Issue.js
@@ -1,21 +1,59 @@
 import React from "react";
 
 import cssClass from "./Issue.css";
+import { makeStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '70%',
+    margin: '25px auto',
+    padding: 0,
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '33.33%',
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
+}));
 
 const issue = props => {
+
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
+
     return (
-        <div className={cssClass.Issue}>
-            <div className={cssClass.Detail}>
-                <strong>Name:</strong> {props.issueName}
-            </div>
-            <div className={cssClass.Body}>
-                <strong>Sub Issue:</strong> {props.issueBody}
-            </div>
-            <div className={cssClass.Detail}>
-                <strong>On:</strong>{" "}
-                {new Date(props.publishedOn).toDateString()}
-            </div>
-        </div>
+      <div>
+      <div className={classes.root}>
+      <ExpansionPanel expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+        >
+          <Typography className={classes.heading}>{props.issueName}</Typography>
+          <Typography className={classes.secondaryHeading}>{new Date(props.publishedOn).toDateString()}</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            {props.issueBody}
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      </div>
+    </div>
     );
 };
 
