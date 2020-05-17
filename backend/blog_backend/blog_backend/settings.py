d10dc81c8fe596865b77d1d73af5292afc703de4
RajuAgt <raju.agt501@gmail.com>
2020-05-17 23:50:46 +0530

    Deploy settings

--- backend/blog_backend/blog_backend/settings.py
+++ backend/blog_backend/blog_backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import datetime
+import django-heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -158,3 +159,6 @@ REST_FRAMEWORK = {
     ),
 
 }
+
+# Activate Django-heroku
+django_heroku.settings(locals())
